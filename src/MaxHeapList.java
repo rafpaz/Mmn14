a9f10339185c9f0f165d404820cd5682d3db9a84
Refael Paz <solimer213@walla.co.il>
2015-04-25 18:55:49 +0300

    Testing use of list instead of array

---
+++ src/MaxHeapList.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * Created by Refael_Paz on 25/04/2015.
+ */
+public class MaxHeapList
+{
+    private List<MaxHeap> m_maxHeapList;
+
+    public void AddMaxHeap(int[] num_arr)
+    {
+        MaxHeap tmpList = new MaxHeap(num_arr.length);
+        for (int i : num_arr)
+        {
+            tmpList.insert(i);
+        }
+        m_maxHeapList.add(tmpList);
+    }
+
+
+}
--- src/main.java
+++ src/main.java
@@ -6,19 +6,31 @@ class main
 {
     public static void main(String[] args)
     {
-        MaxHeapArr maxHeapArr = new MaxHeapArr(3);
+        MaxHeapArr maxHeapArr = new MaxHeapArr(2);
         int[] num_arr = {1,2,3,4,5,6};
         int[] num_arr2 = {6,9,-1,7,5,6};
 
         maxHeapArr.InitializeArr(0, num_arr);
-        maxHeapArr.PrintArr(0);
-        System.out.println(maxHeapArr.GetMax(0));
-        maxHeapArr.PrintArr(0);
-        maxHeapArr.Insert(0, 80);
-        maxHeapArr.PrintArr(0);
+        maxHeapArr.InitializeArr(1, num_arr2);
 
-        System.out.println(maxHeapArr.GetMax(0));
-        maxHeapArr.PrintArr(0);
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+        System.out.println(maxHeapArr.GetTotalMax());
+
+        //maxHeapArr.PrintArr(0);
+
+        //System.out.println(maxHeapArr.GetTotalMax());
+
 
 
     }
