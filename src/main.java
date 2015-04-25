8e7a112bc5744d4217bf42b61d6b3d734252c3f5
Refael Paz <solimer213@walla.co.il>
2015-04-25 19:56:27 +0300

    New main

--- src/main.java
+++ src/main.java
@@ -6,20 +6,14 @@ class main
 {
     public static void main(String[] args)
     {
-        MaxHeapArr maxHeapArr = new MaxHeapArr(3);
-        int[] num_arr = {1,2,3,4,5,6};
-        int[] num_arr2 = {6,9,-1,7,5,6};
+        MaxHeapArr maxHeapArr = new MaxHeapArr(2);
+        int[] num_arr = {6,5,3,4,2,1};
+        int[] num_arr2 = {6,9,-1,7};
 
         maxHeapArr.InitializeArr(0, num_arr);
-        maxHeapArr.PrintArr(0);
-        System.out.println(maxHeapArr.GetMax(0));
-        maxHeapArr.PrintArr(0);
-        maxHeapArr.Insert(0, 80);
-        maxHeapArr.PrintArr(0);
+        maxHeapArr.InitializeArr(1, num_arr2);
 
-        System.out.println(maxHeapArr.GetMax(0));
-        maxHeapArr.PrintArr(0);
-
+        maxHeapArr.PrintSortedArray();
 
     }
 
